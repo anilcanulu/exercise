bd4224388d3dc19b5e50816d87c1a7a637dae28d
Alexandra Ministeru <a.ministeru@tudelft.nl>
2024-11-06 16:27:42 +0100

    Fix subtract function

--- test_subtract.py
+++ test_subtract.py
@@ -1,5 +1,5 @@
 def subtract(a, b):
-    return a + b  # do not change this line until prompted to do so.
+    return a - b  # do not change this line until prompted to do so.
 
 def test_subtract():
-	assert add(2,3) == 5
+	assert subtract(2,3) == -1
